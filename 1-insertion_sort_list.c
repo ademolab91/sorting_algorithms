519cc05648a72d74ac626d317732330ad876809b
baaioy <balotech514@gmail.com>
2022-07-27 22:39:20 +0100

    Hot fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,44 +11,36 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	if (*list)
-	{
-		listint_t *current = *list, *hole;
-		size_t k = 0, swapped, i, j;
+	listint_t *current, *temp;
 
-		while (current)
+	if (!(*list))
+		return;
+
+	current = *list;
+	while (current)
+	{
+		while (current->next && (current->n > current->next->n))
 		{
-			k++;
-			current = current->next;
-		}
-		for (i = 1; i <= k; i++)
-		{
-			current = *list;
-			swapped = 0;
-			for (j = 1; j <= (i - 1); j++)
-				current = current->next;
-			hole = current;
-			while (hole && hole->prev && ((hole->prev)->n > current->n))
-			{
-				if (!swapped)
-				{
-					(hole->prev)->next = hole->next;
-					(hole->next)->prev = hole->prev;
-					swapped = 1;
-				}
-				hole = hole->prev;
-			}
-			if (swapped)
-			{
-				current->next = hole;
-				current->prev = hole->prev;
-				hole->prev = current;
-				if (current->prev)
-					(current->prev)->next = current;
-				else
-					*list = current;
-			}
+			temp = current->next;
+			current->next = temp->next;
+			temp->prev = current->prev;
+
+			if (current->prev)
+				current->prev->next = temp;
+
+			if (temp->next)
+				temp->next->prev = current;
+
+			current->prev = temp;
+			temp->next = current;
+
+			if (temp->prev)
+				current = temp->prev;
+			else
+				*list = temp;
+
 			print_list(*list);
 		}
+		current = current->next;
 	}
 }
